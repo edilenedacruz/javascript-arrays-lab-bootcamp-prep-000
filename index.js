398bb714ae6f7e9564796d422d531ee95e6aed0a
Edilene Da Cruz <hello@edilene.io>
2017-10-21 20:41:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,8 @@ function appendKitten(name) {
   var moreKittens = [...kittens, name]
   return moreKittens
 }
+
+function prependKitten(name) {
+  var moreKittens = [name, ...kittens]
+  return moreKittens
+}
